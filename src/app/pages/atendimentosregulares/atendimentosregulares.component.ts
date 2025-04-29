d0cb55a222fbe387ba5b0bed0b7654fbf221ff2d
LucasMKonopka <150478119+LucasMKonopka@users.noreply.github.com>
2025-04-29 20:43:04 -0300

    calendario consultas dias anteriores e comecei a tela de consultas regulares

--- src/app/pages/atendimentosregulares/atendimentosregulares.component.ts
+++ src/app/pages/atendimentosregulares/atendimentosregulares.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AtendimentosService } from '../../services/atendimentos.service';
+import { ToastrService } from 'ngx-toastr';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { PacientesService } from '../../services/pacientes.service';
+
 
 @Component({
   selector: 'app-atendimentosregulares',
   templateUrl: './atendimentosregulares.component.html',
   styleUrl: './atendimentosregulares.component.scss'
 })
-export class AtendimentosregularesComponent {
+export class AtendimentosregularesComponent implements OnInit{
+  paciente: any = null;
+  idPaciente!: string;
+  consultaForm: FormGroup;
+
+  constructor(
+      private fb: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+      private atendimentosService: AtendimentosService,
+      private toastr: ToastrService,
+      private afAuth: AngularFireAuth,
+      private pacientesService: PacientesService
+    ) {
+      
+    }
+
+
+  ngOnInit(): void {
+    this.idPaciente = this.route.snapshot.paramMap.get('id')!;
+    
+    this.pacientesService.getPacienteById(this.idPaciente).subscribe(paciente => {
+      this.paciente = paciente;
+    });
+
+    
+  }
+
+  calcularIdade(dataNascimento: string): number {
+    if (!dataNascimento) return 0;
+    const nasc = new Date(dataNascimento);
+    const hoje = new Date();
+    let idade = hoje.getFullYear() - nasc.getFullYear();
+    const m = hoje.getMonth() - nasc.getMonth();
+    if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) {
+      idade--;
+    }
+    return idade;
+  }
 
 }
