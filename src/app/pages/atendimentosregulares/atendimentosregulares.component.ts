e4af9803bcd9fd480c687f478bacfe1450782f95
LucasMKonopka <150478119+LucasMKonopka@users.noreply.github.com>
2025-05-05 08:47:44 -0300

    cabeçalho consultas regulares

--- src/app/pages/atendimentosregulares/atendimentosregulares.component.ts
+++ src/app/pages/atendimentosregulares/atendimentosregulares.component.ts
@@ -5,6 +5,7 @@ import { AtendimentosService } from '../../services/atendimentos.service';
 import { ToastrService } from 'ngx-toastr';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { PacientesService } from '../../services/pacientes.service';
+import { firstValueFrom } from 'rxjs';
 
 
 @Component({
@@ -16,6 +17,7 @@ export class AtendimentosregularesComponent implements OnInit{
   paciente: any = null;
   idPaciente!: string;
   consultaForm: FormGroup;
+  informacoesIniciais: any = null;
 
   constructor(
       private fb: FormBuilder,
@@ -24,21 +26,29 @@ export class AtendimentosregularesComponent implements OnInit{
       private atendimentosService: AtendimentosService,
       private toastr: ToastrService,
       private afAuth: AngularFireAuth,
-      private pacientesService: PacientesService
+      private pacientesService: PacientesService,
     ) {
       
     }
 
 
-  ngOnInit(): void {
-    this.idPaciente = this.route.snapshot.paramMap.get('id')!;
+    async ngOnInit(): Promise<void> {
+      this.idPaciente = this.route.snapshot.paramMap.get('id')!;
     
-    this.pacientesService.getPacienteById(this.idPaciente).subscribe(paciente => {
-      this.paciente = paciente;
-    });
-
+      try {
+        this.paciente = await firstValueFrom(this.pacientesService.getPacienteById(this.idPaciente));
+        
+        const consultas = await this.atendimentosService.getConsultasPorPaciente(this.idPaciente);
+        const primeiraConsulta = consultas.find((consulta: any) => consulta.tipo === 'primeira');
     
-  }
+        if (primeiraConsulta) {
+          this.informacoesIniciais = primeiraConsulta;
+        }
+      } catch (error) {
+        console.error('Erro ao carregar dados do paciente ou da primeira consulta', error);
+        this.toastr.error('Erro ao carregar informações do paciente');
+      }
+    }
 
   calcularIdade(dataNascimento: string): number {
     if (!dataNascimento) return 0;
